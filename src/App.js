cd6cbcfed2361d96937ff866c3c3d7bcb5cacefb
Chris <chrishethertonweb@gmail.com>
2022-11-02 19:03:25 +0100

    added Modal to the index file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import Header from "./components/layout/Header";
 import Meals from "./components/meals/Meals";
+import Cart from "./components/cart/Cart";
 
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
